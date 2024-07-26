bc6b1953e8ba0b7b21814dbbea665e809cdf7531
Dante Huaman <danraf77@gmail.com>
2024-07-26 12:01:34 -0500

    index

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,54 @@
     <div class="p-4 bg-gray-100">
       <ul class="space-y-2">
         <li>
-          <a href="tvperuhd" class="text-blue-600 hover:underline" target="_blank">TV Peru HD</a>
+          <a
+            href="tvperuhd"
+            class="text-blue-600 hover:underline"
+            target="_blank"
+            >TV Peru HD</a
+          >
         </li>
         <li>
-          <a href="tvperunoticias" class="text-blue-600 hover:underline" target="_blank">TV Peru Noticias</a>
+          <a
+            href="tvperunoticias"
+            class="text-blue-600 hover:underline"
+            target="_blank"
+            >TV Peru Noticias</a
+          >
         </li>
         <li>
-          <a href="tvperuinternacional" class="text-blue-600 hover:underline" target="_blank">TV Peru Internacional</a>
+          <a
+            href="tvperuinternacional"
+            class="text-blue-600 hover:underline"
+            target="_blank"
+            >TV Peru Internacional</a
+          >
         </li>
         <li>
-          <a href="tvperuespecial" class="text-blue-600 hover:underline" target="_blank">TV Peru Internacional</a>
+          <a
+            href="tvperuespecial"
+            class="text-blue-600 hover:underline"
+            target="_blank"
+            >TV Peru Señal especial</a
+          >
         </li>
         <li>
-          <a href="canalipe" class="text-blue-600 hover:underline" target="_blank">Canal Ipe</a>
+          <a
+            href="canalipe"
+            class="text-blue-600 hover:underline"
+            target="_blank"
+            >Canal Ipe</a
+          >
         </li>
         <li>
-          <a href="radionacional" class="text-blue-600 hover:underline" target="_blank">Radio Nacional</a>
+          <a
+            href="radionacional"
+            class="text-blue-600 hover:underline"
+            target="_blank"
+            >Radio Nacional</a
+          >
         </li>
       </ul>
     </div>
-
   </div>
 </template>
-
