501228dad1265b091729f31cb69de1dcd15e3051
Dante Huaman <danraf77@gmail.com>
2024-11-06 12:59:20 -0500

    ok

--- server/api/video/[id].get.ts
+++ server/api/video/[id].get.ts
@@ -1,26 +1,30 @@
-import SomeService from "@mux/mux-node";
+// server/api/video/[id].js
+
+import { useRuntimeConfig } from "#imports";
 
 export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  const videoId = event.context.params.id;
   const MUX_TOKEN_ID = "5b09d574-64ce-481f-815a-6d44f28a6dcb";
   const MUX_TOKEN_SECRET =
     "WZgWJLSshm0WXP77nzMpy5HijL3G/6yw7AaDsU2MHwAaZOiecbjIJwzWjecALTJVWulADxu7Nef";
-  const videoId = event.context.params.id;
 
-  const client = new SomeService(MUX_TOKEN_ID, MUX_TOKEN_SECRET);
+  const authHeader = `Basic ${Buffer.from(
+    `${MUX_TOKEN_ID}:${MUX_TOKEN_SECRET}`
+  ).toString("base64")}`;
 
   try {
-    const video = await client.video.Assets.get(videoId);
-    const playbackId = video.playback_ids?.find(
-      (pb) => pb.policy === "public"
-    )?.id;
-
-    if (!playbackId) {
-      return { status: "error", message: "Playback ID no encontrado" };
-    }
-
-    return { status: "success", playbackId };
+    const response = await $fetch(
+      `https://api.mux.com/video/v1/assets/${videoId}`,
+      {
+        headers: {
+          Authorization: authHeader,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return response;
   } catch (error) {
-    console.error("Error al obtener el Playback ID:", error);
-    return { status: "error", message: "Error al obtener el Playback ID" };
+    return { error: error.message };
   }
 });
