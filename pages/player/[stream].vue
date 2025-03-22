c9030970a589fc060b5339774969d3e3aae454d0
Dante Huaman <danraf77@gmail.com>
2025-03-22 10:19:10 -0500

    d

---
+++ pages/player/[stream].vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="streamUrl">
+    <VideoPlayer :streamUrl="streamUrl" :videoId="stream" />
+  </div>
+  <div v-else>
+    <p>El canal solicitado no existe o el stream es inválido.</p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+import VideoPlayer from "@/components/VideoPlayer.vue";
+
+const route = useRoute();
+const stream = route.params.stream;
+
+// Armamos la URL del stream con base fija + parámetro dinámico
+const streamUrl = stream ? `https://cdnhd.iblups.com/hls/${stream}.m3u8` : "";
+
+definePageMeta({
+  layout: false,
+});
+</script>
